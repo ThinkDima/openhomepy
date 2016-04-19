7c9a56674196916677cf1eaa5153a483b16b1e5d
Igor Kachmaryk <igor.kachmaryk@dealertrack.com>
2016-04-19 14:21:53 -0400

    Added error message to connect page

--- core/forms.py
+++ core/forms.py
@@ -4,13 +4,11 @@ from .models import LogicalDevice
 class ConnectForm(forms.ModelForm):
     class Meta:
         model = LogicalDevice
-        exclude = ['device_status', 'connected', 'physical_device']
+        exclude = ['device_status', 'connected', 'physical_device', 'device_id', 'device_type']
 
     def __init__(self, *args, **kwargs):
         super(ConnectForm, self).__init__(*args, **kwargs)
         instance = getattr(self, 'instance', None)
         if instance and instance.pk:
-            self.fields['device_id'].widget.attrs['readonly'] = True
-            self.fields['device_type'].widget.attrs['readonly'] = True
             self.fields['device_name'].widget.attrs['autofocus'] = True
             self.fields['device_name'].widget.attrs['class'] = 'form-control'

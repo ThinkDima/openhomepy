ae14be4cb9b0bd4aafcf0b3a84717756d891d9aa
Igor Kachmaryk <igor.kachmaryk@dealertrack.com>
2016-04-19 14:53:04 -0400

    Added form validation to check if device name already exists

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import LogicalDevice
+from django.forms.utils import ErrorList
 
 class ConnectForm(forms.ModelForm):
     class Meta:
@@ -12,3 +13,11 @@ class ConnectForm(forms.ModelForm):
         if instance and instance.pk:
             self.fields['device_name'].widget.attrs['autofocus'] = True
             self.fields['device_name'].widget.attrs['class'] = 'form-control'
+
+    def clean(self):
+        cleaned_data = super(ConnectForm, self).clean()
+        device_name = cleaned_data.get('device_name')
+        if LogicalDevice.objects.filter(device_name__iexact=device_name).count() > 0:
+            self._errors['device_name'] = ErrorList(['Name Already Exists!'])
+
+        return cleaned_data

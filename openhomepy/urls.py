a573b4febba9534346e01729b6332a848d44bac6
ThinkDima <Think.Dima@gmail.com>
2016-04-19 09:41:15 -0400

    Connect to models

--- openhomepy/urls.py
+++ openhomepy/urls.py
@@ -8,8 +8,8 @@ from django.views.generic import TemplateView
 from core.views import HomepageView, ConnectView
 
 urlpatterns = [
-    url(r'^$', HomepageView.as_view(template_name='index.html'), name='dashboard'),
-    url(r'connect/$', ConnectView.as_view(template_name='connect.html'), name='connect'),
+    url(r'^$', HomepageView.as_view(), name='dashboard'),
+    url(r'connect/$', ConnectView.as_view(), name='connect'),
     url(r'about/$', TemplateView.as_view(template_name='about.html'), name='about'),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
